57539b3d7dde12e797bc3b893dc75a7ffbe58d6d
Ville <visavola@gmail.com>
2024-12-08 13:51:49 +0200

    Solve day 2

--- 2015/day01/main.cpp
+++ 2015/day01/main.cpp
@@ -7,6 +7,8 @@ int main()
     fstream file("input.txt");
     string line;
     int floor = 0;
+    int firstCharPosition = 0;
+    bool isFound = false;
     if (!file)
     {
         cerr << "Error";
@@ -19,9 +21,14 @@ int main()
             } else{
                 floor--;
             }
+            if(floor == -1 && !isFound){
+                isFound = true;
+                firstCharPosition = i+1;
+            }
         }
     }
     cout << "PART ONE: " << floor << '\n';
+    cout << "PART TWO: " << firstCharPosition << '\n';
     file.close();
     return 0;
 }
