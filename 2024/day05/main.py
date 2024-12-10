502f762f3d1ebd9afa11fd894353932e70b39a53
Ville Savolainen <ville.savolainen@laskuni.onmicrosoft.com>
2024-12-10 19:15:03 +0200

    solve day 5

--- 2024/day05/main.py
+++ 2024/day05/main.py
@@ -4,6 +4,8 @@ lines = file.readlines()
 rules = []
 pageNumbers = []
 midCount = 0
+correctedMidCount = 0
+inCorrects = []
 
 for line in lines:
     if len(line) > 0 and len(line) <= 6 and len(line) != 1:
@@ -33,5 +35,34 @@ def checkOrder(page):
 for page in pageNumbers:
     if checkOrder(page):
         midCount += int(page[int((len(page)) / 2)])
+    else:
+        inCorrects.append(page)
+
+
+def orderPages(inCorrect):
+    indexOne = 0
+    indexTwo = 0
+    midNum = 0
+    for rule in rules:
+        if rule[0] in inCorrect and rule[1] in inCorrect:
+            for i in range(len(inCorrect)):
+                if int(inCorrect[i]) == int(rule[0]):
+                    indexOne = i
+                if int(inCorrect[i]) == int(rule[1]):
+                    indexTwo = i
+            if indexOne > indexTwo:
+                temp = inCorrect[indexOne]
+                inCorrect[indexOne] = inCorrect[indexTwo]
+                inCorrect[indexTwo] = temp
+                orderPages(inCorrect)
+            else:
+                midNum = int(inCorrect[int((len(inCorrect)) / 2)])
+    return midNum
+                
+
+for page in inCorrects:
+    correctedMidCount += orderPages(page)
         
+
 print("PART ONE ", midCount)
+print("PART TWO ", correctedMidCount)
